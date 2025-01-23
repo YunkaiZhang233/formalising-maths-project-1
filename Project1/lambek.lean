6265d2876abb2b60739b7a5e3687d5a3880ff00b
Yunkai Zhang <yunkai.zhang22@imperial.ac.uk>
2025-01-23 15:51:19 +0000

    completed basic homomorphism definition

--- Project1/lambek.lean
+++ Project1/lambek.lean
@@ -2,7 +2,7 @@ import Mathlib.Tactic
 
 set_option autoImplicit false
 
--- namespace Initial
+namespace Initial
 open CategoryTheory
 
 universe u'
@@ -12,11 +12,12 @@ variable {C : Type u'} [Category.{v'} C]
 class InitialObject (X : C) : Prop where
   unique_morphism : ∀ (Y : C), ∃ (f : X ⟶ Y), ∀ (g : X ⟶ Y), f = g
 
--- end Initial
+end Initial
 
 
-universe u
-universe v
+namespace CategoryTheory
+
+universe u v
 
 variable {C : Type u} [Category.{v} C]
 
@@ -25,3 +26,64 @@ structure FAlgebra (F : C ⥤ C) where
   carrier : C
   /-- the arrow -/
   mor : F.obj carrier ⟶ carrier
+
+namespace FAlgebra
+
+local notation:80 g " ⊚ " f:80 => f ≫ g    -- type as \oo
+
+
+variable {F : C ⥤ C} -- (A : FAlgebra F){B C : FAlgebra F}
+
+/-- Define that all F-Algebra form a category.
+This include components:
+* homomorphisms: `h : (A, α) ⟶ (B, β)` is essentially an arrow `h : A ⟶ B`
+  such that `h ∘ α = β ∘ F (h)`
+* identities: identity arrows
+
+```
+         F h
+   F A -----> F B
+    |         |
+  α |         | β
+    V         V
+    A  -----> B
+        h
+```
+-/
+
+structure AlgebraHom (A B : FAlgebra F) where
+  -- mathching carrier
+  h : A.carrier ⟶ B.carrier
+  --
+  condition : h ⊚ A.mor = B.mor ⊚ (F.map h)
+
+variable (A : FAlgebra F){A' B' C': FAlgebra F}
+
+
+namespace AlgebraHom
+
+def id : AlgebraHom A A where
+  h := 𝟙 _
+  condition := by
+    aesop
+
+def comp (m1: AlgebraHom A' B') (m2: AlgebraHom B' C') : AlgebraHom A' C' where
+  h := m1.h ≫ m2.h
+  condition := by
+    simp [Functor.map_comp]
+    rw [← m2.condition]
+    simp [← Category.assoc]
+    rw [m1.condition]
+
+end AlgebraHom
+
+instance (F : C ⥤ C) : CategoryStruct (FAlgebra F) where
+  Hom := AlgebraHom
+  id := AlgebraHom.id -- (X : FAlgebra F) → X ⟶ X
+  comp := AlgebraHom.comp -- {X Y Z : FAlgebra F} → (X ⟶ Y) → (Y ⟶ Z) → (X ⟶ Z)
+--
+
+
+end FAlgebra
+
+end CategoryTheory
